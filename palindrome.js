09bdddb36f531bb4d9c83913209ec8554daec906
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-21 18:29:29 +0530

    string and number palindrome

--- palindrome.js
+++ palindrome.js
@@ -10,3 +10,18 @@ if(string==rev){
 } else {
     console.log(`false ${rev}`);
 }
+
+let n = 1234
+let revN = 0;
+
+while(n>0){
+    let rem = n%10;
+    revN = revN*10 + rem;
+    n = Math.floor(n/10);
+}
+console.log(revN)
+if(n==revN){
+    console.log("Number is Palindrome")
+} else {
+    console.log("Not a Palindrome");
+}
