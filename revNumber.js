70e3064d9bf51dd5b8cfb8db62f45681d2f4e5e8
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-21 18:29:03 +0530

    revNUmber

--- revNumber.js
+++ revNumber.js
@@ -1,8 +1,10 @@
-let number = 12345;
-let rev=0;
-while( number > 0){
-    a = number%10;
-    rev =rev*10 + a;
-    console.log(rev)
-    number = Math.floor(number/10);
+let n = 12345;
+let rev = 0;
+
+while(n>0){
+    let rem = n%10;
+    rev = rev*10 + rem;
+    n = Math.floor(n/10);
 }
+
+console.log(rev)
