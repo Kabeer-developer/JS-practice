93d353d0538eda5c0054ce16c9052331b9b82789
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-22 16:41:25 +0530

    TwoSum

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,15 @@
 function twoSum(array,target){
     let map = {};
     for(let i=0;i<array.length;i++){
-        diff = target - array[i];
-        if(map[diff] !== undefined) return [map[diff],i]
-        map[array[i]] = i
+        let diff = target - arr[i]
+        if(map[diff] !== undefined) {
+            return [map[diff],i]
+        }
+        map[array[i]]=i
     }
 }
 
 const arr = [1,2,3,4,5,6,7,8,9]
-const goal = 16
+const goal = 10
 const result = twoSum(arr,goal);
 console.log(result)
