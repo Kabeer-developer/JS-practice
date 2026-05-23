481a0d48c3bca8e40ccc76de9486115c20b5358c
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-23 11:40:11 +0530

    Capitalize

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,20 @@
 //Capitalize first letter of a string 
 let string = "hello"
-let result = string[0].toUppercase;
+let result = string[0].toUpperCase();
 console.log(result)
+
+// Capitalize first letter of each word
+
+let sentence = "hello my name is kabeer";
+let words = sentence.split(" ");
+let op="";
+
+// let name = "kabeer"
+// let op = name[0].toUpperCase()+name.slice(1);
+// console.log(op);
+
+for(let word of words){
+    let capitalize = word.charAt(0).toUpperCase()+word.slice(1);
+    op += capitalize+" ";
+}
+console.log(op)
