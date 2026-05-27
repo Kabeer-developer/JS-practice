b8ec3a48a01eaf8d8aaad3c7d8c53dd7f631e72d
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-27 17:46:47 +0530

    Curry fn

--- curry.js
+++ curry.js
@@ -1,9 +1,9 @@
 function curry(a,b){
     if(b!==undefined){
         return a+b;
-    }
-    return function subCurry(b){
-        return a+b;
+    }  
+       return function subcurry(b){
+        return a+b
     }
 }
 
