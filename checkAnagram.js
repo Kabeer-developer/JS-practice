8bf465e82b622b256fc0e9a3d1cd17db7069f606
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-05 19:21:01 +0530

    Anagram

--- checkAnagram.js
+++ checkAnagram.js
@@ -1,9 +1,12 @@
-let str1 = "mad"
-// let str2 = "dam";
-let str2 = "mab"
- if (str1.split("").sort().join("")===str2.split("").sort().join("")){
-    console.log("Strings are Anagram");
- } else {
-    console.log("Strings are not Anagram")
- }
+let str1 = "dam";
+let str2 = "mad";
 
+function checkAnagram(str1,str2){
+    isAnagram = false;
+    if(str1.split("").sort().join()===str2.split("").sort().join()){
+        isAnagram =true;
+    }
+    return isAnagram;
+}
+
+console.log(checkAnagram(str1,str2));
