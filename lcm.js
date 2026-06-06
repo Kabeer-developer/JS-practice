605de027a39b3b5deefb4401134620f43f4e4892
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-06 19:09:05 +0530

    LCM

--- lcm.js
+++ lcm.js
@@ -1,15 +1,12 @@
-// HCF/GCD
-let a =8,b=12;
-let min = a>b?b:a;
-let gcd=0;
-for(let i=1;i<=min;i++){
-    if(a%i ==0 && b%i==0){
-        if(i>gcd){
-            gcd =i
-        }
+console.log("GCD/HCF");
+function gcd(a, b) {
+  let min = a > b ? b : a;
+  for (let i = min; i >= 1; i--) {
+    if (a % i == 0 && b % i == 0) {
+      return i;
     }
+  }
 }
-console.log(gcd);
-
-let lcm = (a*b)/gcd;
-console.log(lcm)
+let a = 10,b=20;
+let lcm = (a*b)/gcd(a,b);
+console.log(lcm);
