792266d8f378e8dc7cc65a2a292701a23430253c
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-06 19:08:38 +0530

    Palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,14 @@
 function checkPalindrome(str){
     isPalindrome = true;
     let n = str.length;
-    for(let i=0;i<=Math.ceil(n/2);i++){
+    for(let i=0;i<=Math.floor(n/2);i++){
         if(str[i]!=str[n-1-i]){
-            isPalindrome=false;
+            isPalindrome = false;
             break;
         }
     }
     return isPalindrome;
 }
 
-console.log(checkPalindrome("Hello"));
+console.log(checkPalindrome("kabeer"));
+console.log(checkPalindrome("madam"));
