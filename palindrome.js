c7e02d5aa8b2d1bfef17e7a3051c1771f62287a5
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-15 19:10:43 +0530

    Palindrome

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,7 @@ function checkPalindrome(str){
     isPalindrome = true;
     let n = str.length;
     for(let i=0;i<=Math.floor(n/2);i++){
-        if(str[i]!=str[n-1-i]){
+        if(str[i]!== str[n-1-i]){
             isPalindrome = false;
             break;
         }
