248d93b97418d9faee5c71d068c4207f26a9bd0f
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-16 18:54:31 +0530

    Largest of Arr

--- largestofArray.js
+++ largestofArray.js
@@ -1,9 +1,9 @@
-let array = [1,2,4,3,7,6,5]
+let array=[1,2,3,4,5,6,7,6,5,4,3];
 let max = array[0];
 
-for(let i=0; i<array.length;i++){
+for(let i=0;i<array.length;i++){
     if(array[i]>max){
         max = array[i];
     }
 }
-console.log(max)
+console.log(max);
