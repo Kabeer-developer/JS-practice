d565eb3735cf81216b7ad660c3e36fd06311e1c4
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-16 18:53:36 +0530

    missing number

--- missingNumber.js
+++ missingNumber.js
@@ -1,8 +1,10 @@
 let arr = [1,2,3,5,6,4,8];
 
-let expectedSum ,currentSum;
+let expSum , curSum =0;
+
 let n = arr.length+1;
-expectedSum = (n*(n+1)/2);
-currentSum = arr.reduce((a,b)=> a+b,0)
-const missing = expectedSum-currentSum;
-console.log(missing);
+
+curSum = arr.reduce((a,b)=> a+b,0);
+expSum = (n*(n+1))/2;
+let missingNum = expSum-curSum;
+console.log(missingNum);
