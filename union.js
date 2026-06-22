d32fff76f810ea32eacec59752e78c1a421de3a0
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-22 20:14:21 +0530

    Arr union

--- union.js
+++ union.js
@@ -1,5 +1,8 @@
 let arr1 = [1,2,3,4,5,5,4,3], arr2 = [6,7,8,9,10];
-let result;
 
-result =[...new Set([...arr1,...arr2])];
-console.log(result);
+for(let i=0;i<arr2.length;i++){
+    arr1.push(arr2[i]);
+}
+
+console.log(arr1);
+
