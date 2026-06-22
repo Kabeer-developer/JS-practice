a873542b6805fb36fd2a68f5eae0e4ab6333c7e1
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-22 20:14:46 +0530

    Zeros to end

--- zerosToEnd.js
+++ zerosToEnd.js
@@ -2,12 +2,15 @@ let arr = [1,2,0,3,4,0,5,6,0,7,6];
 let result=[];
 
 for(let i=0;i<arr.length;i++){
-    if(arr[i]!==0){
+    if(arr[i]!=0){
         result.push(arr[i]);
     }
 }
-let zeros = arr.length-result.length;
-for(let i=0;i<zeros;i++){
-    result.push(0)
+
+let numZero = arr.length-result.length;
+
+for(let i=1;i<=numZero;i++){
+    result.push(0);
 }
+
 console.log(result);
