874155ea324c971d087426611aa388ebfdb5ee3a
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-23 19:24:43 +0530

    Deep Clone

--- deepClone.js
+++ deepClone.js
@@ -1,14 +1,16 @@
-const deepclone = (obj)=> {
-    if(obj === null || typeof obj !== "object") return obj;
+const deepClone = (obj)=> {
+    if (obj == null || typeof obj !== "object") return;
 
     if(Array.isArray(object)){
-        return obj.map((item)=> deepclone(item));
+        return obj.map((item)=> deepClone(obj));
     }
 
     let result = {}
-    for(let key in obj){
-        result[key] = deepclone(obj[key]);
+
+    for (let key in obj){
+        result[key] = deepClone(obj[key]);
     }
+
     return result;
 }
 
@@ -16,9 +18,12 @@ const object = {
     a : 10
 }
 const b =object;
-// b.a=300;
+const c = structuredClone(object);
+c.a=200;
+console.log(c.a);
+b.a=300;
 console.log(b.a);
-const clone = deepclone(object);
+const clone = deepClone(object);
 clone.a = 100;
 console.log(object.a);
 console.log(clone.a);
