db4516b82fcdb58770bf7e0574b4d99e817f6069
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-23 19:00:28 +0530

    BubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,18 +3,13 @@ function bubbleSort(arr){
     for(let i=0;i<n;i++){
         for(let j=0;j<n-i;j++){
             if(arr[j]>arr[j+1]){
-            let temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp;
             }
         }
     }
     return arr;
 }
-let array = [5,4,3,6,7,2,21];
-console.log(bubbleSort(array));
-console.log(array.sort((a,b)=> a-b));
-// second largest
-let sLargest = bubbleSort(array);
-let sLen = sLargest.length;
-console.log(sLargest[sLen-2]);
+
+console.log(bubbleSort([1,2,5,4,3,6,9,8,7]));
