d4401f54068f26ba7695e2ad1d5567b21a5acf4c
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-27 18:16:10 +0530

    Chunck array

--- chunckArray.js
+++ chunckArray.js
@@ -1,7 +1,7 @@
 let array = [1,2,3,4,5,6,7,8];
 
 function chunckArray(arr,size){
-    let result = [];
+    let result =[]
     for(let i=0;i<arr.length;i+=size){
         result.push(arr.slice(i,i+size));
     }
