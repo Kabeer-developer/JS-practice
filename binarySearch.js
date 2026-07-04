4b16fdd2caf3688347d1d9f2c416c7495c5538eb
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-04 18:05:24 +0530

    Binary Search

--- binarySearch.js
+++ binarySearch.js
@@ -1,17 +1,13 @@
-
 function binarySearch(arr,target){
-  let low =0;
-  let high = arr.length-1;
-
+  let low = 0, high = arr.length-1;
   while(low<=high){
     let mid = Math.floor((low+high)/2);
     if(arr[mid]==target){
       return mid;
-    }
-    if(target>arr[mid]){
+    } else if(target>arr[mid]){
       low=mid+1;
-    } else {
-      high = mid-1;
+    } else{
+      high=mid-1;
     }
   }
   return -1;
