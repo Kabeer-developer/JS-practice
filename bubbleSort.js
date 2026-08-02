47044d6a8a7f7f31f748caf8ec4c283c1c3067ce
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-02 18:13:53 +0530

    BubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,11 @@
 function bubbleSort(arr){
-    let n=arr.length-1;
+    let n = arr.length-1;
     for(let i=0;i<n;i++){
-        for(j=0;j<n-i;j++){
+        for(let j=0;j<n-i;j++){
             if(arr[j]>arr[j+1]){
-                let temp = arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
